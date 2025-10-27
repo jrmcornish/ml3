f115eebd917566e945d1fe28df898a7f7426ee1e
Rob Cornish <jrmcornish@gmail.com>
2025-10-27 18:47:36 +0800

    Temporary commit - WIP

--- Ml3/Main.lean
+++ Ml3/Main.lean
@@ -1,5 +1,166 @@
 import Mathlib.Data.NNReal.Basic
 
+
+structure Iso (α β : Sort u) : Type u where
+  (f : α → β)
+  (g : β → α)
+  (left: ∀ x, g (f x) = x)
+  (right: ∀ y, f (g y) = y)
+
+-- TODO: need these to be monads
+axiom Closed : Sort u → Sort u
+axiom Open : Sort u → Sort u
+
+axiom ClosedIdempotent : ∀ (α : Sort u), Iso (Closed (Closed α)) (Closed α)
+axiom OpenIdempotent : ∀ (α : Sort u), Iso (Open (Open α)) (Open α)
+
+def CloseU.{u} := Σ τ : Sort u, Iso (Closed τ) τ
+def OpenU.{u} := Σ τ : Sort u, Iso (Open τ) τ
+
+noncomputable def ClosedFactor (α : Sort u) : CloseU.{u} :=
+  ⟨Closed α, ClosedIdempotent α⟩
+
+noncomputable def OpenFactor (α : Sort u) : OpenU.{u} :=
+  ⟨Open α, OpenIdempotent α⟩
+
+class OpenMonad (M : OpenU → OpenU) where
+  pure : α.fst → (M α).fst
+  bind : (M α).fst → (α.fst → (M β).fst) → (M β).fst
+
+class ClosedMonad (M : CloseU → CloseU) where
+  pure : α.fst → (M α).fst
+  bind : (M α).fst → (α.fst → (M β).fst) → (M β).fst
+
+-- This is not the elementary definition of monad morphism T1 => T2 defined on
+-- the same category. Instead it uses a more general notion defined when T1 and
+-- T2 are monads on different categories C1 and C2 (see Definition 3.1 of
+-- https://ncatlab.org/nlab/show/monad#BicategoryOfMonads).
+--
+-- Specifically, given T1 : Monad C1 and T2 : Monad C2, the data of a monad
+-- morphism T1 => T2 consists of:
+--   * A functor F : C1 -> C2
+--   * A natural transformation T2 ∘ F  => F ∘ T1
+--   * A proof that the monad laws hold
+--
+-- In our case, we take C1 = OpenU, C2 = CloseU, and F = ClosedFactor ∘ fst
+-- (where here fst is just the inclusion of OpenU into Sort u).
+def MonadMorphism (T1 : CloseU → CloseU) [ClosedMonad T1] (T2 : OpenU → OpenU) [OpenMonad T2] :=
+  (A : OpenU) → (T1 (ClosedFactor A.fst)).fst → (ClosedFactor (T2 A).fst).fst
+
+axiom fracture : Iso (Sort u) (Σ' (A : CloseU.{u}) (B : OpenU.{u}), A.fst → (Closed B.fst))
+
+-- Problem: How can we do (mFunctorAction M g)
+def GluedMonad
+  {M : CloseU → CloseU} [ClosedMonad M]
+  {N : OpenU → OpenU} [OpenMonad N]
+  (m : MonadMorphism M N)
+  (A : Type u) : Type u :=
+    let ⟨X, Y, g⟩ := fracture.f A
+    sorry
+    -- fracture.g (M X.fst, N Y.fst, fun x => m ((mFunctorAction M g) x))
+
+-- ----
+
+-- def MonadMorphism (T1 T2 : Type u → Type u) := (A : OpenU) → T1 (Closed A.fst) → Closed (T2 A.fst)
+
+-- def mFunctorAction {A B} (M : Type u → Type u) [Monad M] (f : A → B) : M A → M B := fun m => do
+--   let x <- m
+--   return (f x)
+
+
+-- def ClosedMonad (M : Type u → Type u) [Monad M] := ∀ A : CloseU, Iso (Closed (M A.fst)) (M A.fst)
+-- def OpenMonad (M : Type u → Type u) [Monad M] := ∀ A : OpenU, Iso (Open (M A.fst)) (M A.fst)
+
+-- -- def GluedMonad {M : Type u → Type u} {N : Type u → Type u} [Monad M] [Monad N] (p : OpenMonad N) (m: MonadMorphism M N) (A : Type u) : Type u :=
+-- --   let ⟨X, Y, g⟩ := fracture.f A
+-- --   let ⟨toIsoM, fromIsoM, _, _⟩ := p Y
+-- --   fracture.g (M X.fst, N Y.fst, fun x => m ((mFunctorAction M g) x))
+
+-- -- Problem: How can we do (mFunctorAction M g)
+-- def GluedMonad {M : Type u → Type u} {N : Type u → Type u} [Monad M] [Monad N] (p : OpenMonad N) (m: MonadMorphism M N) (A : Type u) : Type u := sorry
+
+-------
+
+def X := ℝ
+def Y := ℝ
+def G := ℝ
+
+-- Signature
+
+inductive SymSig (A : Type) : Type where
+| ret : A → SymSig A
+| α : G → X → (X → SymSig A) → SymSig A
+| β : G → Y → (Y → SymSig A) → SymSig A
+| inv : G → (G → SymSig A) → SymSig A
+| f : X → (Y → SymSig A) → SymSig A
+| γ : X → (G → SymSig A) → SymSig A
+
+namespace SymSig
+
+def SymBind {A B : Type} (x : SymSig A) (h : A → SymSig B) : SymSig B :=
+  match x with
+  | ret a => h a
+  | α g x k => α g x (fun x' => SymBind (k x') h)
+  | β g y k => β g y (fun y' => SymBind (k y') h)
+  | inv g k => inv g (fun g' => SymBind (k g') h)
+  | f x k => f x (fun y => SymBind (k y) h)
+  | γ x k => γ x (fun g' => SymBind (k g') h)
+
+instance : Monad SymSig where
+  pure := ret
+  bind := SymBind
+
+-- Generic effects
+def α_op (g : G) (x : X) : SymSig X := α g x (fun x' => ret x')
+def β_op (g : G) (y : Y) : SymSig Y := β g y (fun y' => ret y')
+def inv_op (g : G) : SymSig G := inv g (fun g' => ret g')
+def f_op (x : X) : SymSig Y := f x (fun y => ret y)
+def γ_op (x : X) : SymSig G := γ x (fun g' => ret g')
+
+--- Math monad
+
+def IdentityMonad (A : Type) : Type := A
+
+instance : Monad IdentityMonad where
+  pure := fun x => x
+  bind := fun m f => f m
+
+--- monad morphisms
+
+def monad_morph : MonadMorphism SymSig IdentityMonad := fun A m =>
+  match m with
+  | ret a => a
+  | α g x k => k x
+  | β g y k => k y
+  | inv g k => k g
+  | f x k => k (0 : Y)  -- placeholder
+  | γ x k => k (0 : G)  -- placeholder
+
+
+--- use fracture axiom to glue monads
+
+def GluedSymSig (A : Type) : Type := GluedMonad monad_morph A
+#check GluedSymSig
+
+instance : Monad GluedSymSig where
+  pure := sorry
+  bind := sorry
+
+def model (x : X) : SymSig Y :=
+  do
+    let g <- γ_op x
+    let g' <- inv_op g
+    let x' <- α_op g' x
+    let y <- f_op x'
+
+    let y' <- β_op g y
+    return y'
+
+end SymSig
+
+
+--------
+
 open scoped NNReal
 
 #check ℝ≥0
@@ -28,16 +189,11 @@ lemma math_works : (prog (fun n => if n = 0 then 1 else 0) = 1/4) :=
   simp
   ring
 
---------
 
-axiom closed : Sort u → Sort u
 
-def close_universe : Type u := Sort u -> Sort u
+-----
 
-def equiv (α β) := exists (f : α → β) (g : β → α), (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y)
-def equiv' (α β) := exists (f : α → β) (g : β → α), (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y)
 
-axiom fracture : equiv (Sort u) (Σ' (A : Sort u) (B : Sort u), A → (closed B))
 
 inductive MDP (A : Type) : Type where
 | ret : A → MDP A
@@ -48,7 +204,7 @@ open MDP
 
 noncomputable def flipKont : Unit → K Bool := fun () k => 1/2 * (k True + k False)
 
-noncomputable def denSem {α} : MDP α → K α := fun x =>
+noncomputable def denSem α : MDP α → K α := fun x =>
 match x with
  | ret a => return a
  | MDP.flip k => let F1 := denSem (k True)
@@ -83,10 +239,10 @@ lemma unitLaw {A} : (fun x : InputOutput A => KleisliExtension ret' x) = (fun x 
 
 def ioProg : InputOutput Unit := input (fun n => if n > 3 then output 3 (ret' ()) else ret' ())
 
-
 ----
-
-
 
 def signature := List (Type × Type)
 def fromSigToFunctor (σ : signature) : Type -> Type := sorry
+
+
+----
