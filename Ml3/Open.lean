77d00251ad1460a71de770bece384a84a8cd92e5
Pedro Amorim <pedrohenrique.a.a@gmail.com>
2025-10-30 06:38:30 +0000

    made construction parametric

--- Ml3/Open.lean
+++ Ml3/Open.lean
@@ -1,20 +1,23 @@
-axiom p : Prop
+import Ml3.Postulates
+
+section OpenModality
+variable (p : Prop)
 
 def Open (τ : Sort u) : Sort u := p → τ
 
-def OpenUnit {τ : Sort u} (t : τ) : Open τ :=
+def OpenUnit {τ : Sort u} (t : τ) : Open p τ :=
   fun _ => t
 
-def OpenBind {τ τ' : Sort u} (f : τ → Open τ') (t : Open τ) : Open τ' :=
+def OpenBind {τ τ' : Sort u} (f : τ → Open p τ') (t : Open p τ) : Open p τ' :=
   fun ph => f (t ph) ph
 
-def OpenFunctor {τ τ' : Sort u} (f : τ → τ') (t : Open τ) : Open τ' :=
-  OpenBind (fun t' => OpenUnit (f t')) t
+def OpenFunctor {τ τ' : Sort u} (f : τ → τ') (t : Open p τ) : Open p τ' :=
+  OpenBind p (fun t' => OpenUnit p (f t')) t
 
-def OpenMultiplication {τ : Sort u} (t : Open (Open τ)) : Open τ :=
-  OpenBind (fun t : Open τ => t) t
+def OpenMultiplication {τ : Sort u} (t : Open p (Open p τ)) : Open p τ :=
+  OpenBind p (fun t : Open p τ => t) t
 
-theorem OpenRightUnitLaw {τ : Sort u} : ∀ t : Open τ, OpenMultiplication (OpenUnit t) = t := by
+theorem OpenRightUnitLaw {τ : Sort u} : ∀ t : Open p τ, OpenMultiplication p (OpenUnit p t) = t := by
   intro t
   unfold OpenMultiplication OpenUnit OpenBind
   -- We should be careful about simp here: we want to make sure this doesn't use
@@ -35,11 +38,12 @@ theorem OpenRightUnitLaw {τ : Sort u} : ∀ t : Open τ, OpenMultiplication (Op
 def IsIso {α β : Sort u} (f : α → β) : Prop :=
   ∃ g : β → α, (∀ x, g (f x) = x) ∧ (∀ y, f (g y) = y)
 
-theorem OpenIdempotent {τ : Sort u} : IsIso (OpenFunctor (OpenUnit (τ := τ))) := by
-  refine ⟨OpenMultiplication (τ := τ), ?p⟩
+theorem OpenIdempotent {τ : Sort u} : IsIso (OpenFunctor p (OpenUnit p (τ := τ))) := by
+  refine ⟨OpenMultiplication p (τ := τ), ?p⟩
   constructor
   . intro x
     apply OpenRightUnitLaw
   . intro y
     unfold OpenFunctor OpenUnit OpenMultiplication OpenBind
     simp
+end OpenModality
